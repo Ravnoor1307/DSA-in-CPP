c983349b53522aadd32d50159f1f9dd93270fa38
Ravnoor Singh <1986ravnoorsingh8the@gmail.com>
2026-08-09 23:05:10 +0530

    Miscelleaneous_new

--- 01_C++_Basics/09_Miscellaneous.cpp
+++ 01_C++_Basics/09_Miscellaneous.cpp
@@ -3,38 +3,38 @@ using namespace std;
 int main()
 {
 
-    int a = 4;
-    int b = 8;
+     int a = 4;
+     int b = 8;
 
-    // Bitwise Operators
+     // Bitwise Operators
 
-    cout << "Bitwise & of " << a << " and " << b << " : " << (a & b) << endl;
-    cout << "Bitwise | of " << a << " and " << b << " : " << (a | b) << endl;
-    cout << "Bitwise ^ of " << a << " and " << b << " : " << (a ^ b) << endl;
-    cout << "Bitwise << of  (a<<1)" << " : " << (a << 1) << endl;
-    cout << "Bitwise >> of  (a>>1)" << " : " << (a >> 1) << endl;
+     cout << "Bitwise & of " << a << " and " << b << " : " << (a & b) << endl;
+     cout << "Bitwise | of " << a << " and " << b << " : " << (a | b) << endl;
+     cout << "Bitwise ^ of " << a << " and " << b << " : " << (a ^ b) << endl;
+     cout << "Bitwise << of  (a<<1)" << " : " << (a << 1) << endl;
+     cout << "Bitwise >> of  (a>>1)" << " : " << (a >> 1) << endl;
 
-    // Data Modifiers
-    cout << endl
-         << sizeof(int) << endl;
-    cout << endl
-         << sizeof(long int) << endl;
-    cout << endl
-         << sizeof(short int) << endl;
-    cout << endl
-         << sizeof(long long int) << endl;
+     // Data Modifiers
+     cout << endl
+          << sizeof(int) << endl;
+     cout << endl
+          << sizeof(long int) << endl;
+     cout << endl
+          << sizeof(short int) << endl;
+     cout << endl
+          << sizeof(long long int) << endl;
 
-    unsigned int c = -10;
-    cout << endl
-         << c << endl;
+     unsigned int c = -10;
+     cout << endl
+          << c << endl;
 
-    // NOTE:
-    // Assigning a negative value to an unsigned type in C++ does not store a negative number.
-    // Instead, the value is converted using modulo 2^n arithmetic, where n is the number of bits.
-    // For a 32-bit unsigned int:
-    //   -10 becomes: 2^32 - 10 = 4294967286
-    // This is defined by the C++ standard (integer conversion rules) and is not an error.
-    // Be careful: such implicit conversions can cause unexpected wrap-around results.
+     // NOTE:
+     // Assigning a negative value to an unsigned type in C++ does not store a negative number.
+     // Instead, the value is converted using modulo 2^n arithmetic, where n is the number of bits.
+     // For a 32-bit unsigned int:
+     //   -10 becomes: 2^32 - 10 = 4294967286
+     // This is defined by the C++ standard (integer conversion rules) and is not an error.
+     // Be careful: such implicit conversions can cause unexpected wrap-around results.
 
-    return 0;
+     return 0;
 }
